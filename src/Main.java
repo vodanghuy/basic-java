abf4f9d7db609db7bea37ac6a903824b65df9406
Vo Dang Huy <iamdaghuy@gmail.com>
2025-11-12 14:19:39 +0700

    Weight Conversion Program

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,54 @@
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.Scanner;
-import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Queue<Integer> superPrimeNumber = new LinkedList<>();
-        int input = scanner.nextInt();
-        for(int i = 2; i <= input && i <= 9; i++){
-            if(isPrime(i)){
-                superPrimeNumber.add(i);
+        int option;
+        double weight;
+        String tryAgain = "yes";
+        System.out.println("Weight Conversion Program");
+        System.out.println("1: Convert kgs to lbs");
+        System.out.println("2: Convert lbs to kgs");
+        do{
+            System.out.print("Choose an option: ");
+            // Stop user enter a letter
+            while (!scanner.hasNextInt())
+            {
+                System.out.println("Invalid choice! Try again!");
+                System.out.print("Choose an option: ");
+                scanner.next();
             }
-        }
-        while (!superPrimeNumber.isEmpty()){
-            int front = superPrimeNumber.peek();
-            for(int i = 1; i <= 9; i++){
-                int k = front*10 + i;
-                if(k <= input && isPrime(k)){
-                    superPrimeNumber.add(k);
+            option = scanner.nextInt();
+
+            switch (option){
+                case 1 -> {
+                    System.out.print("Enter the weight in kgs: ");
+                    weight = scanner.nextDouble();
+                    weight *= 2.20462;
+                    System.out.printf("The new weight in lbs is: %.2f\n", weight);
+                }
+                case 2 -> {
+                    System.out.print("Enter the weight in lbs: ");
+                    weight = scanner.nextDouble();
+                    weight /= 2.20462;
+                    System.out.printf("The new weight in kgs is: %.2f\n", weight);
+                }
+                default -> {
+                    System.out.println("Invalid choice! Try again");
+                    continue;
                 }
             }
-            System.out.print(superPrimeNumber.peek() + " ");
-            superPrimeNumber.poll();
-        }
-    }
-
-    static boolean isPrime(int n){
-        if(n < 2){
-            return false;
-        }
-        for(int i = 2; i <= Math.sqrt(n); i++){
-            if(n % i == 0){
-                return false;
-            }
-        }
-        return true;
+            scanner.nextLine();
+            do {
+                System.out.print("Try again? (YES/NO): ");
+                tryAgain = scanner.nextLine();
+                if(!tryAgain.equalsIgnoreCase("yes") && !tryAgain.equalsIgnoreCase("no"))
+                {
+                    System.out.println("Invalid choice! Try again!");
+                }
+            }while (!tryAgain.equalsIgnoreCase("yes") && !tryAgain.equalsIgnoreCase("no"));
+        }while (tryAgain.equalsIgnoreCase("yes"));
+        System.out.println("SEE YOU NEXT TIME!");
+        scanner.close();
     }
 }
